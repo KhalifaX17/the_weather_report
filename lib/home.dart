3e8e184bd1ccf3857f17da20e67aaa2a2863afd6
KhalifaX17 <teaoak350@gmail.com>
2025-12-22 13:42:38 +0700

    main

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:the_weather_report/services/open_weather_services.dart';
@@ -12,15 +13,20 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    var apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
     return Scaffold(
       appBar: AppBar(title: Text("Weather Report")),
       body: FutureBuilder(
-        future: OpenWeatherService.fetch(apiKey!),
+        future: OpenWeatherService().fetch(),
         builder: ((context, snapshot) {
           List<Widget> list = [];
           if (snapshot.hasData) {
             var data = snapshot.data!;
+            var img = CachedNetworkImage(
+              imageUrl:
+                  "https://openweathermap.org/img/wn/${data.weather![0].icon}.png",
+              progressIndicatorBuilder: (context, url, progress) =>
+                  Center(child: Text("watting")),
+            );
             list.add(
               Center(
                 child: Image.network(
--- lib/services/open_weather_services.dart
+++ lib/services/open_weather_services.dart
@@ -1,13 +1,29 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:the_weather_report/types/open_weather.dart';
 
 class OpenWeatherService {
-  static Future<OpenWeather> fetch(String apiKey) async {
-    var position = await OpenWeatherService.determineCurrentPosition();
-    var url =
-        "https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=$apiKey";
+  // Private named constructor
+  OpenWeatherService._internal();
+
+  // The single instance (lazy initialization)
+  static final OpenWeatherService _instance = OpenWeatherService._internal();
+
+  // Factory constructor returns the same instance every time
+  factory OpenWeatherService() {
+    return _instance;
+  }
+
+  String get baseUrl {
+    var apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
+    return "https://api.openweathermap.org/data/2.5/weather?appid=$apiKey";
+  }
+
+  Future<OpenWeather> fetch() async {
+    var position = await determineCurrentPosition();
+    var url = "$baseUrl&lat=${position.latitude}&lon=${position.longitude}";
     var result = await http.get(Uri.parse(url));
     if (result.statusCode != 200) {
       return Future.error("Failed to fetch weather data");
@@ -16,7 +32,7 @@ class OpenWeatherService {
     return OpenWeather.fromJson(objectResult);
   }
 
-  static Future<Position> determineCurrentPosition() async {
+  Future<Position> determineCurrentPosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
